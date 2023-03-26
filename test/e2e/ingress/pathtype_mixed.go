630b150a4a7520af204e55d7e8cb2f4a7ab21045
George Gooden <gecgooden@gmail.com>
2023-03-26 12:32:10 +0800

    Add failing e2e test

--- test/e2e/ingress/pathtype_mixed.go
+++ test/e2e/ingress/pathtype_mixed.go
@@ -24,6 +24,7 @@ import (
 	"github.com/stretchr/testify/assert"
 
 	networking "k8s.io/api/networking/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/ingress-nginx/test/e2e/framework"
 )
 
@@ -142,5 +143,138 @@ var _ = framework.IngressNginxDescribe("[Ingress] [PathType] mix Exact and Prefi
 
 		assert.Contains(ginkgo.GinkgoT(), body, "pathtype=prefix")
 		assert.Contains(ginkgo.GinkgoT(), body, "pathheader=/")
+	})
+
+	ginkgo.It("should prioritize exact over prefix when matching path", func() {
+		host := "my.domain.com"
+		prefixPathType := networking.PathTypePrefix
+		exactPathType := networking.PathTypeExact
+		implementationSpecificPathType := networking.PathTypeImplementationSpecific
+
+		f.NewEchoDeployment(framework.WithDeploymentName("current-frontend"))
+		f.NewEchoDeployment(framework.WithDeploymentName("new-service"))
+
+		currentFrontendIngress := &networking.Ingress{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "current-frontend",
+				Namespace: f.Namespace,
+			},
+			Spec: networking.IngressSpec{
+				IngressClassName: framework.GetIngressClassName(f.Namespace),
+				Rules: []networking.IngressRule{
+					{
+						Host: host,
+						IngressRuleValue: networking.IngressRuleValue{
+							HTTP: &networking.HTTPIngressRuleValue{
+								Paths: []networking.HTTPIngressPath{
+									{
+										Path:     "/",
+										PathType: &prefixPathType,
+										Backend: networking.IngressBackend{
+											Service: &networking.IngressServiceBackend{
+												Name: "current-frontend",
+												Port: networking.ServiceBackendPort{
+													Number: 80,
+												},
+											},
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		}
+
+		newServiceIngress := &networking.Ingress{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "new-service",
+				Namespace: f.Namespace,
+			},
+			Spec: networking.IngressSpec{
+				IngressClassName: framework.GetIngressClassName(f.Namespace),
+				Rules: []networking.IngressRule{
+					{
+						Host: host,
+						IngressRuleValue: networking.IngressRuleValue{
+							HTTP: &networking.HTTPIngressRuleValue{
+								Paths: []networking.HTTPIngressPath{
+									{
+										Path:     "/someendpoint",
+										PathType: &implementationSpecificPathType,
+										Backend: networking.IngressBackend{
+											Service: &networking.IngressServiceBackend{
+												Name: "new-service",
+												Port: networking.ServiceBackendPort{
+													Number: 80,
+												},
+											},
+										},
+									},
+									{
+										Path:     "/",
+										PathType: &exactPathType,
+										Backend: networking.IngressBackend{
+											Service: &networking.IngressServiceBackend{
+												Name: "new-service",
+												Port: networking.ServiceBackendPort{
+													Number: 80,
+												},
+											},
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		}
+
+		f.EnsureIngress(currentFrontendIngress)
+		f.EnsureIngress(newServiceIngress)
+
+		f.WaitForNginxServer(host,
+			func(server string) bool {
+				return strings.Contains(server, "current-frontend") &&
+					strings.Contains(server, "new-service")
+			})
+
+		ginkgo.By("Check /someendpoint request to hit new-service")
+		body := f.HTTPTestClient().
+			GET("/someendpoint").
+			WithHeader("Host", host).
+			Expect().
+			Status(http.StatusOK).
+			Body().
+			Raw()
+
+		assert.Contains(ginkgo.GinkgoT(), body, "Hostname: new-service")
+		assert.NotContains(ginkgo.GinkgoT(), body, "Hostname: current-frontend")
+
+		ginkgo.By("Check /somethingelse request to hit current-frontend")
+		body = f.HTTPTestClient().
+			GET("/somethingelse").
+			WithHeader("Host", host).
+			Expect().
+			Status(http.StatusOK).
+			Body().
+			Raw()
+
+		assert.NotContains(ginkgo.GinkgoT(), body, "Hostname: new-service")
+		assert.Contains(ginkgo.GinkgoT(), body, "Hostname: current-frontend")
+
+		ginkgo.By("Check / request to hit new-service")
+		body = f.HTTPTestClient().
+			GET("/").
+			WithHeader("Host", host).
+			Expect().
+			Status(http.StatusOK).
+			Body().
+			Raw()
+
+		assert.Contains(ginkgo.GinkgoT(), body, "Hostname: new-service")
+		assert.NotContains(ginkgo.GinkgoT(), body, "Hostname: current-frontend")
 	})
 })
